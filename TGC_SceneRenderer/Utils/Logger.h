c04ce6b75510eccf5e358707af4fd9353dfe06ee
jose-villegas <polonoise@gmail.com>
2014-12-09 01:34:15 -0430

    missing header

---
+++ TGC_SceneRenderer/Utils/Logger.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Utils {
+    public ref class Logger {
+        private:
+            static System::Collections::ArrayList Log;
+            static System::Windows::Forms::ToolStripStatusLabel ^windowStatusLabel;
+        public:
+            static void Write(System::String ^text);
+            static void Write(System::String ^text, bool writeToStatusLabel);
+            static void Write(System::String ^text, bool writeToStatusLabel, System::Drawing::Color labelForeColor);
+            static void ClearLog();
+            static void SetStatusLabel(System::Windows::Forms::ToolStripStatusLabel ^statusLabel);
+            static System::String ^GetLastLog();
+    };
+}
+
